46be43eb7087f52266f8efcfc1cb2b8fd3d9d2f9
AWM-PRO <113374252+AWM-PRO@users.noreply.github.com>
2022-09-19 12:22:40 +0430

    Add files via upload

---
+++ sex.py
@@ -0,0 +1,42 @@
+import os,sys
+os.system('clear')
+print ("""
+        88888888ba  8b        d8  88
+        88      "8b  Y8,    ,8P   88
+        88      ,8P   `8b  d8"    88
+        88_____-8P"     Y88P      88
+        88------8b,     d88b      88
+        88      `8b   ,8P  Y8,    88
+        88      a8P  d8"    `8b   88
+        88888888P"  8P        Y8  88
+--------------------------------------------------
+ Owner      : Binyamin Binni
+ Cracked By : mamunXten + shantoXten
+ Status     : Free
+ Version    : 3.3
+--------------------------------------------------
+[01] Start Tool
+[02] Update
+[03] Get Bxi App
+[00] Exit
+--------------------------------------------------""")
+xxx = input(' [!] Select : ')
+
+if xxx in ('1'):
+      os.system('clear')
+      os.system('chmod 777 bypass && ./bypass')
+
+elif xxx in ('2'):
+      os.system('git pull')
+      os.system('clear && python sex.py')
+elif xxx in ('3'):
+      print ("""Download App From Here :
+
+https://drive.google.com/file/d/1CjlHfmMNocI44mmtN-bC1pXztrw-YJdT/view?usp=sharing\n""")
+      xx = input('Press Enter To Start Tool again')
+      os.system('python sex.py')
+
+elif xxx in ('00'):
+      os.system('exit')
+else:
+     os.system('clear && python sex.py')
