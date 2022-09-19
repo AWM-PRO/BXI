421c7af3ac385a20aa911dbab78a8993b8daf742
AWM-PRO <113374252+AWM-PRO@users.noreply.github.com>
2022-09-19 19:45:10 +0430

    Update AWM-BXI.py

--- AWM-BXI.py
+++ AWM-BXI.py
@@ -11,9 +11,10 @@ print ("""
         88888888P"  8P        Y8  88
 --------------------------------------------------
  Owner      : Binyamin Binni
- Cracked By : Mirwais + Skipper
+ Cracked By : Mirwais + Skipper A.W.M TEAM
  Status     : Premium
  Version    : 3.3
+ Note       : Do not update the app, it will Data be deleted and we will not be responsible. It is completely safe
 --------------------------------------------------
 [01] Start Tool
 [02] Update
@@ -30,9 +31,9 @@ elif xxx in ('2'):
       os.system('git pull')
       os.system('clear && python sex.py')
 elif xxx in ('3'):
-      print ("""Download App From Here :
+      print ("""Download App From Telegram :
 
-https://drive.google.com/file/d/1CjlHfmMNocI44mmtN-bC1pXztrw-YJdT/view?usp=sharing\n""")
+https://t.me/AWM_TEAMPRO/12
       xx = input('Press Enter To Start Tool again')
       os.system('python sex.py')
 
