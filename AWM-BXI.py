c87266233437adc73e69f1f28757f4f804f538c4
AWM-PRO <113374252+AWM-PRO@users.noreply.github.com>
2022-09-19 19:52:22 +0430

    Create AWM-BXI.py

---
+++ AWM-BXI.py
@@ -0,0 +1,44 @@
+import os,sys
+os.system('clear')
+print ("""
+        88888888ba  8b        d8  88
+        88      "8b  Y8,    ,8P   88
+        88      ,8P   `8b  d8"    88
+        88_____-8P"     Y88P      88
+        88------8b,     d88b      88
+        88      `8b   ,8P  Y8,    88
+        88      a8P  d8"    `8b   88
+        88888888P"  8P        Y8  88
+--------------------------------------------------
+ Owner      : Binyamin Binni
+ Cracked By : Mirwais + Skipper AWM TEAM
+ Status     : Premium 
+ Version    : 3.3
+ Note j.       :Do not update the app, it will Data be deleted and we will not be responsible. It is completely safe
+--------------------------------------------------
+[01] Start Tool
+[02] Update
+[03] Get Bxi App
+[00] ExitExit
+
+--------------------------------------------------""")
+xxx = input(' [!] Select : ')
+
+if xxx in ('1'):
+      os.system('clear')
+      os.system('chmod 777 bypass && ./bypass')
+
+elif xxx in ('2'):
+      os.system('git pull')
+      os.system('clear && python sex.py')
+elif xxx in ('3'):
+      print ("""Download App From Here :
+
+https://t.me/AWM_TEAMPRO/12\n""")
+      xx = input('Press Enter To Start Tool again')
+      os.system('python sex.py')
+
+elif xxx in ('00'):
+      os.system('exit')
+else:
+     os.system('clear && python sex.py')
